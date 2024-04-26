c634cbe9ae69ebb35369566551ea01f164e314fe
hcruzgar <hcruzgar@emeal.nttdata.com>
2024-04-25 20:29:06 -0500

    Se realizo el debouncedValue en las busqueda

--- ReactNative_app/src/presentacion/screens/Home/HomeScreen.tsx
+++ ReactNative_app/src/presentacion/screens/Home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {FAB, Text, useTheme} from 'react-native-paper';
 import {getPokemons} from '../../../actions/pokemons';
 import {useInfiniteQuery, useQueryClient} from '@tanstack/react-query';
 import {PokeballBg} from '../../components/ui/PokeballBg';
@@ -8,8 +8,12 @@ import {FlatList} from 'react-native-gesture-handler';
 import {globalTheme} from '../../../config/theme/global-theme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {PokemonCard} from '../../components/pokemons/PokemonCard';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../../navigator/StackNavigator';
 
-export const HomeScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'HomeScreen'> {}
+export const HomeScreen = ({navigation}: Props) => {
+  const theme = useTheme();
   // Queries
   const {top} = useSafeAreaInsets();
   const queryClient = useQueryClient();
@@ -41,12 +45,19 @@ export const HomeScreen = () => {
         data={data?.pages.flat() ?? []}
         keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
         numColumns={2}
-        style={{paddingTop: top + 20}}
+        style={{paddingTop: top}}
         ListHeaderComponent={() => <Text variant="displayMedium">Pokédex</Text>}
         renderItem={({item}) => <PokemonCard pokemon={item} />}
         onEndReachedThreshold={0.6}
         onEndReached={() => fetchNextPage()}
         showsVerticalScrollIndicator={false}
+      />
+      <FAB
+        label="Buscar"
+        style={[globalTheme.fab, {backgroundColor: theme.colors.primary}]}
+        mode="elevated"
+        color={theme.dark ? 'black' : 'white'}
+        onPress={() => navigation.push('SearchScreen')}
       />
     </View>
   );
