25ff730d93010589c914a66a2ed331714d15cb9b
hcruzgar <hcruzgar@emeal.nttdata.com>
2024-04-25 19:03:23 -0500

    Se finaliza el diseño de la pantalla detalle

--- ReactNative_app/src/domain/entities/pokemon.ts
+++ ReactNative_app/src/domain/entities/pokemon.ts
@@ -7,4 +7,19 @@ export interface Pokemon {
 
   //TODO:
   color: string;
+
+  games: string[];
+  stats: Stat[];
+  abilities: string[];
+  moves: Move[];
+}
+
+export interface Stat {
+  name: string;
+  value: number;
+}
+
+export interface Move {
+  name: string;
+  level: number;
 }
